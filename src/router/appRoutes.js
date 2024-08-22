699f047215298fdbd079df344485cf5c0f66885b
Deepak4617 <dk4803382@gmail.com>
2024-08-22 21:52:39 +0530

    add new

--- src/router/appRoutes.js
+++ src/router/appRoutes.js
@@ -3,12 +3,14 @@ import AdminPage from '../pages/adminPage';
 import GuestPage from '../pages/guestPage';
 
 const AppRoute = () => {
+
   let routes = useRoutes([
     { path: '/admin', element: <AdminPage /> },
     { path: '/', element: <GuestPage /> },
   ]);
 
   return routes;
+  
 };
 
 export default AppRoute;
