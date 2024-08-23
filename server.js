79155ca9044e3b0f7c0bdbf4b525a579f93f541f
Deepak4617 <dk4803382@gmail.com>
2024-08-23 11:46:13 +0530

    add

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
