69b7e77a1e92612fbcef477f24b27a3f9bd6c9e0
Deepak4617 <dk4803382@gmail.com>
2024-08-23 12:07:35 +0530

    add new

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
+// const express = require('express');
+// const http = require('http');
+// const socketIo = require('socket.io');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(cors({ origin: 'http://localhost:3000' }));
+
+// const server = http.createServer(app);
+// const io = socketIo(server, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//   },
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('New client connected');
+
+//   socket.on('buttonPress', (buttonId) => {
+//     io.emit('buttonPress', buttonId);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('Client disconnected');
+//   });
+// });
+
+// server.listen(4000, () => {
+//   console.log('Server is running on port 4000');
+// });
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+
+// Update CORS settings to include Netlify domain
+app.use(cors({
+  origin: 'https://deepak--phasertask1234.netlify.app',
+}));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://deepak--phasertask1234.netlify.app',
     methods: ['GET', 'POST'],
   },
 });
